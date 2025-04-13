e4f4a7be7bb6ea841dd4f4a0710b50ba48b67866
mailbau <muhammad.ismail.azmi@mail.ugm.ac.id>
2025-04-14 02:47:31 +0700

    auth

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
-from controllers import auth_controller
+from controllers import auth_controller, user_controller
 
 auth_bp = Blueprint("auth_bp", __name__)
 
-auth_bp.route("/register", methods=["POST"])(auth_controller.register)
-auth_bp.route("/login", methods=["POST"])(auth_controller.login)
+auth_bp.route("/api/register", methods=["POST"])(auth_controller.register)
+auth_bp.route("/api/login", methods=["POST"])(auth_controller.login)
+auth_bp.route("/api/request-reset", methods=["POST"])(user_controller.request_password_reset)
+auth_bp.route("/api/reset-password", methods=["POST"])(user_controller.reset_password)
