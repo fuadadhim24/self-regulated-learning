3e5ea1932df74b332ffd4d8b5b8086aa8e3c3c13
fuadadhim24 <fuadadhim24@gmail.com>
2025-10-20 18:58:44 +0700

    feat: add vercel insight

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,11 +2,17 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { loadAccessTokenFromStorage } from "@/utils/auth";
 import { useEffect } from "react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     loadAccessTokenFromStorage();
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <SpeedInsights />
+      <Component {...pageProps} />
+    </>
+  );
 }
